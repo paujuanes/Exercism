a23b818149ae1b32bb566941145886db92a04f37
paujuanes <54763681+paujuanes@users.noreply.github.com>
2019-10-28 20:21:40 +0100

    New exercise

---
+++ python/robot-name/robot_name.py
@@ -0,0 +1,6 @@
+class Robot(object):
+    def __init__(self):
+        import string, random
+        a = ''.join([random.choice(string.ascii_uppercase) for i in range(2)])
+        b = ''.join([random.choice(string.digits) for i in range(3)])
+        return a+b
