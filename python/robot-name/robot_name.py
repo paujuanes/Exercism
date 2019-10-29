ad91168135185be5df548435350393bad81eaaba
paujuanes <54763681+paujuanes@users.noreply.github.com>
2019-10-29 19:51:05 +0100

    Unfinished

--- python/robot-name/robot_name.py
+++ python/robot-name/robot_name.py
@@ -1,5 +1,7 @@
 class Robot(object):
     def __init__(self):
+        self.reset()
+    def reset(self):
         import string, random
         a = ''.join([random.choice(string.ascii_uppercase) for i in range(2)])
         b = ''.join([random.choice(string.digits) for i in range(3)])
