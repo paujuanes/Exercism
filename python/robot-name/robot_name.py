bcb938de23172687a1518275bc81ed6faff2708e
Pau <54763681+paujuanes@users.noreply.github.com>
2019-11-19 23:04:25 +0100

    Updates

--- python/robot-name/robot_name.py
+++ python/robot-name/robot_name.py
@@ -1,8 +1,19 @@
+import string, random
+
 class Robot(object):
+
     def __init__(self):
         self.reset()
+
     def reset(self):
-        import string, random
+        names = []
         a = ''.join([random.choice(string.ascii_uppercase) for i in range(2)])
         b = ''.join([random.choice(string.digits) for i in range(3)])
-        return a+b
+        name = a+b
+        if name not in names:
+            return name
+        else:
+            self.reset()
+
+    def __del__(self):
+        self.reset()
