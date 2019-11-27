2755db4237cb67fec94bccd2c95de54ffc9f8879
Pau <54763681+paujuanes@users.noreply.github.com>
2019-11-27 19:30:23 +0100

    Exercise completed

--- python/robot-name/robot_name.py
+++ python/robot-name/robot_name.py
@@ -1,19 +1,18 @@
 import string, random
 
-class Robot(object):
+class Robot:
 
     def __init__(self):
-        self.reset()
+        self.name = self.reset()
 
     def reset(self):
+        random.seed()
         names = []
-        a = ''.join([random.choice(string.ascii_uppercase) for i in range(2)])
-        b = ''.join([random.choice(string.digits) for i in range(3)])
-        name = a+b
-        if name not in names:
-            return name
+        a = ''.join(random.choices(string.ascii_uppercase, k=2))
+        b = ''.join(random.choices(string.digits, k=3))
+        self.name = a+b
+        if self.name not in names:
+            names.append(self.name)
+            return self.name
         else:
             self.reset()
-
-    def __del__(self):
-        self.reset()
